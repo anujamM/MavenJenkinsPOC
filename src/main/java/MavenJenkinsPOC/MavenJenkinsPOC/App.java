208d4209bf64f078a00d429e42ba144d0b5fee03
Anujam Mondal <anujam.mondal@cerner.com>
2021-07-09 22:21:44 +0530

    First Commit

---
+++ src/main/java/MavenJenkinsPOC/MavenJenkinsPOC/App.java
@@ -0,0 +1,13 @@
+package MavenJenkinsPOC.MavenJenkinsPOC;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
