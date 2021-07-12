cc3e02dddeb7aca145bcacf5dfc115fb7a980be6
Anujam Mondal <anujam.mondal@cerner.com>
2021-07-12 14:45:36 +0530

    07122021

---
+++ src/test/java/MavenJenkinsPOC/MavenJenkinsPOC/Demo.java
@@ -0,0 +1,33 @@
+package MavenJenkinsPOC.MavenJenkinsPOC;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.Test;
+
+public class Demo {
+static WebDriver driver = null;
+	
+	@Test
+	public static void main() throws InterruptedException {
+		// TODO Auto-generated method stub
+
+		System.setProperty("webdriver.chrome.driver",
+				"C:/Eclipse_Oxy/Drivers/chromedriver_win32/chromedriver.exe");
+		
+		driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		
+//		driver.get("https://www.makemytrip.com/");
+//		driver.findElement(By.xpath("//label[@for='fromCity']")).click();
+//
+//		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+//		WebDriverWait wait = new WebDriverWait(driver, 10);
+//		wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//input[@type='text' and @placeholder='From']"))).sendKeys("Kolkata");
+//		wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//p[contains(text(),'Kolkata')]"))).click();
+		
+		System.out.println(System.getProperty("platformName"));
+	}
+}
