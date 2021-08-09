bf6d978b86ad01944bd45487f8b8f22af45eed91
Anujam Mondal <anujam.mondal@cerner.com>
2021-08-09 19:23:54 +0530

    new

--- src/test/java/MavenJenkinsPOC/MavenJenkinsPOC/Demo.java
+++ src/test/java/MavenJenkinsPOC/MavenJenkinsPOC/Demo.java
@@ -1,12 +1,17 @@
 package MavenJenkinsPOC.MavenJenkinsPOC;
 
-import org.testng.annotations.Test;
+import java.util.concurrent.TimeUnit;
+
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
 import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
 
 public class Demo {
 static WebDriver driver = null;
@@ -23,18 +28,31 @@ static WebDriver driver = null;
 		
 //		driver.get("https://www.makemytrip.com/");
 //		driver.findElement(By.xpath("//label[@for='fromCity']")).click();
-//
-//		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-//		WebDriverWait wait = new WebDriverWait(driver, 10);
+		WebDriverWait wait = new WebDriverWait(driver, 10);
 //		wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//input[@type='text' and @placeholder='From']"))).sendKeys("Kolkata");
 //		wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//p[contains(text(),'Kolkata')]"))).click();
-		try {
-			System.out.println("The platform name is: " + System.getProperty("platformName"));
-		}
-		catch(Exception e){
-			e.printStackTrace();
-			System.out.println("Failureeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee");
-		}
 		
+		
+//		wait.until(ExpectedConditions.refreshed(ExpectedConditions.visibilityOf(driver.findElement(By.id("abc")))));		//Example
+//		
+//		driver.navigate().refresh();		//Example
+//		
+//		SoftAssert soft = new SoftAssert();		//Example
+//		soft.assertEquals("actual", "expected");
+//		Assert.assertEquals("", "");
+//		try {
+//			System.out.println("The platform name is: " + System.getProperty("platformName"));
+//		}
+//		catch(Exception e){
+//			e.printStackTrace();
+//			System.out.println("Failureeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee");
+//		}
+		
+		driver.get("https://www.google.com/");
+		Actions acts = new Actions(driver);
+		Thread.sleep(5000);
+		driver.findElement(By.xpath("//input[@class='gLFyf gsfi']"));
+//		driver.findElement(By.);
+		acts.keyDown(Keys.SHIFT).sendKeys("anujam").keyUp(Keys.SHIFT).build().perform();
 	}
 }
